6bb03f9a305de2e791499b69350599aef57e19db
Zil Norvilis <zilasino27@gmail.com>
2025-01-16 00:47:05 +0200

    Add turbo modal base

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+pin 'tailwindcss-stimulus-components' # @6.1.3
+pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.2
