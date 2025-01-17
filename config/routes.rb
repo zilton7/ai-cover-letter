095cc54f15e68c26b42b43e7c0b6814b6133eff8
Zil Norvilis <zilasino27@gmail.com>
2025-01-17 19:41:05 +0200

    Add design code files

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get 'test_modal', to: 'jobs#test_modal'
+  root to: 'jobs#new'
 
   # authenticate :user, ->(u) { u.admin? } do
   require 'sidekiq/web'
