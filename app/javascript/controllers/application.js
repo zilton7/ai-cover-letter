881a800807203dad0563e6eb53e4bf37643151c5
Zil Norvilis <zilasino27@gmail.com>
2025-01-25 21:30:39 +0200

    WIP

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,13 @@
 import { Application } from "@hotwired/stimulus";
-import { Modal } from "tailwindcss-stimulus-components";
+import { Modal, Dropdown, Alert } from "tailwindcss-stimulus-components";
 import RevealController from "@stimulus-components/reveal";
-import { Dropdown } from "tailwindcss-stimulus-components";
 
 const application = Application.start();
 
 application.register("modal", Modal);
 application.register("reveal", RevealController);
 application.register("dropdown", Dropdown);
+application.register("alert", Alert);
 
 // Configure Stimulus development experience
 application.debug = false;
