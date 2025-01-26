9f9fc66cc89757370f5f778e5b213ea432424911
Zil Norvilis <zilasino27@gmail.com>
2025-01-26 17:57:18 +0200

    update CoverLetterCount  methods

--- app/models/cover_letter.rb
+++ app/models/cover_letter.rb
@@ -12,15 +12,15 @@ class CoverLetter < ApplicationRecord
   private
 
   def increment_cover_letter_count
-    CoverLetterCount.first&.increment!(:count)
+    CoverLetterCount.increment
   end
 
   def decrement_cover_letter_count
-    CoverLetterCount.first&.decrement!(:count)
+    CoverLetterCount.decrement
   end
 
   def display_cover_letters_count
     broadcast_update_to 'cover-letters-count', target: 'cover-letters-count',
-                                               html: CoverLetterCount.first&.count.to_s || 0
+                                               html: CoverLetterCount&.count.to_s || 0
   end
 end
--- app/models/cover_letter_count.rb
+++ app/models/cover_letter_count.rb
@@ -1,2 +1,13 @@
 class CoverLetterCount < ApplicationRecord
+  def self.count
+    CoverLetterCount.first&.count&.positive? ? CoverLetterCount.first.count : 0
+  end
+
+  def self.increment
+    CoverLetterCount.first&.increment!(:count)
+  end
+
+  def self.decrement
+    CoverLetterCount.first&.decrement!(:count)
+  end
 end
