1596d877442419a4e32a5d0227d9b13a05852dd5
Zil Norvilis <zilasino27@gmail.com>
2025-01-27 21:28:06 +0200

    Style up subscription index view

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,12 +4,16 @@ StripeEvent.signing_secret = Rails.application.credentials.dig(:stripe, :signing
 STRIPE_PLANS = {
   premium: {
     id: 'price_1Qlv4yCrHIUGY6dGoN7pPEF9', # Replace with your Stripe Price ID
-    name: 'Unlimited Subscription',
-    price: 277 # $10.00 in cents
+    name: 'Unlimited',
+    description: 'Using Groq AI 🚀',
+    price: 277,
+    most_popular: false
   },
   turbo_premium: {
     id: 'price_1Qlv6jCrHIUGY6dGpikCCVw8', # Replace with your Stripe Price ID
-    name: 'Turbo Unlimited Subscription',
-    price: 577 # $20.00 in cents
+    name: 'Turbo Unlimited',
+    description: 'Using GPT-4 🤖',
+    price: 577,
+    most_popular: true
   }
 }.freeze
