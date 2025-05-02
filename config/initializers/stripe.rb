b95f47802de06ae8218fa4f1608301dc67cd6eb4
Zil Norvilis <zilasino27@gmail.com>
2025-05-02 16:53:21 +0300

    Update Stripe credentials and add premium plan IDs for development and production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,16 +1,19 @@
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :restricted_key)
 StripeEvent.signing_secret = Rails.application.credentials.dig(:stripe, :signing_secret)
 
+premium_plan_id = Rails.application.credentials.dig(:stripe, :premium_plan_id)
+turbo_premium_plan_id = Rails.application.credentials.dig(:stripe, :turbo_premium_plan_id)
+
 STRIPE_PLANS = {
   premium: {
-    id: 'price_1RE8M7CrHIUGY6dG80EdLS7h', # Replace with your Stripe Price ID
+    id: premium_plan_id, # Replace with your Stripe Price ID
     name: 'Unlimited',
     description: 'Using Groq AI 🚀',
     price: 277,
     most_popular: false
   },
   turbo_premium: {
-    id: 'price_1RE8KvCrHIUGY6dGzwCcfvyP', # Replace with your Stripe Price ID
+    id: turbo_premium_plan_id, # Replace with your Stripe Price ID
     name: 'Turbo Unlimited',
     description: 'Using GPT-4 🤖',
     price: 577,
